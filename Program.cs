df1728f6ccbe6c1c8c9373c0dd2b3c2c640f4f11
leand <leand@PROGRAMADOR23>
2023-05-15 20:10:09 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using ConsoleApp6.model;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+cadastro cadastro = new cadastro();
+cadastro.id = 1;
+cadastro.nome = "laercio Pinzam";
+cadastro.educaçao = "ensino superior";
+cadastro.salario = 3.400f;
+cadastro.tempodeservico = 8;
+cadastro.Codigofunçao = 1;
+
+foreach (var item in cadastro) ;
+{
+    Console.WriteLine(cadastro);
+}
+
---
+++ model/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6.model
+{
+    public class cadastro
+    {
+        public int id;
+        public string nome;
+        public string educaçao;
+        public float salario;
+        public float tempodeservico;
+        public int Codigofunçao;
+
+         
+
+
+    }
+    
+    
+       
+    
+}
